fc33e196b65bc95ac3c841d31ddfa71401036eb7
Dani2003G <daniel0202almeida@outlook.com>
2023-09-03 17:44:26 -0300

    GET game by id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GameDTO {
+
+    private Long id;
+
+    private String title;
+
+    private Integer year;
+
+    private String genre;
+
+    private String platforms;
+
+    private BigDecimal score;
+
+    private String imgUrl;
+
+    private String shortDescription;
+
+    private String longDescription;
+
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+}
